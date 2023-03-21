fa6b9f96ca75355766dface9b80f22a43aaa30c7
Atahualpa Arias <atahulpq@gmail.com>
2023-03-21 15:30:39 -0300

    Allow everything cors

--- src/main/java/com/portfolio/backend/CorsConfig.java
+++ src/main/java/com/portfolio/backend/CorsConfig.java
@@ -10,6 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "DELETE")
+            .allowedHeaders("*");
+                ;
     }
 }
