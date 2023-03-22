570773e2c68146ce1234a52d1e2fbab057e4a2e3
Atahualpa Arias <atahulpq@gmail.com>
2023-03-21 22:07:31 -0300

    Allow every source and every request - CORS

--- src/main/java/com/portfolio/backend/CorsConfig.java
+++ src/main/java/com/portfolio/backend/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "DELETE")
+            .allowedMethods("*")
             .allowedHeaders("*");
                 ;
     }
